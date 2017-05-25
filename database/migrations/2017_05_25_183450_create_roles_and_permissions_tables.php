17b6d41c830b70e455dbd552393475469b6db1e5
Soaring Radio <burroughszeb@me.com>
2017-05-25 14:30:35 -0500

    Migrations

--- database/migrations/2017_05_25_183450_create_roles_and_permissions_tables.php
+++ database/migrations/2017_05_25_183450_create_roles_and_permissions_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRolesAndPermissionsTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('display_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
             $table->timestamps();
         });
+
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+
+
     }
 
     /**
@@ -31,6 +36,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('permissions');
     }
 }
---
+++ database/migrations/2017_05_25_190849_create_pivot_tables.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePivotTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('permission_role', function (Blueprint $table) {
+        $table->integer('permission_id')->unsigned()->index();
+        $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+        $table->integer('role_id')->unsigned()->index();
+        $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        $table->primary(['permission_id', 'role_id']);
+      });
+
+      Schema::create('role_user', function (Blueprint $table) {
+        $table->integer('role_id')->unsigned()->index();
+        $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        $table->integer('user_id')->unsigned()->index();
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        $table->primary(['role_id', 'user_id']);
+      });
+
+      Schema::create('role_team', function (Blueprint $table) {
+        $table->integer('role_id')->unsigned()->index();
+        $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        $table->integer('team_id')->unsigned()->index();
+        $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        $table->primary(['role_id', 'team_id']);
+      });
+
+      Schema::create('team_user', function (Blueprint $table) {
+        $table->integer('team_id')->unsigned()->index();
+        $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        $table->integer('user_id')->unsigned()->index();
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        $table->primary(['team_id', 'user_id']);
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('role_team');
+        Schema::dropIfExists('team_user');
+    }
+}
